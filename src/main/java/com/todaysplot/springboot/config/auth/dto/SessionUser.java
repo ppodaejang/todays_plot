c6870205dfe8e5cc6be3d385ec06aae0ee3f6c63
bokim <bo8902@gmail.com>
2024-10-15 14:45:15 +0900

    refactor: Store sessions in database instead of Tomcat memory

--- src/main/java/com/todaysplot/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/todaysplot/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.todaysplot.springboot.config.auth.dto;
 import com.todaysplot.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
     private String name;
     private String email;
